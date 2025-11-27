1e3f7bd2c41c172e6eca404ec113449f439137a6
Alexandre Tavares <alexandre.pinhotavares@gmail.com>
2025-11-27 23:11:21 +0000

    Google tag

---
+++ frontend/src/presentation/components/analytics/google-analytics.tsx
@@ -0,0 +1,27 @@
+import Script from 'next/script'
+
+const GA_TRACKING_ID = 'G-J0E7D0S1FK'
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
+    </>
+  )
+}
+
